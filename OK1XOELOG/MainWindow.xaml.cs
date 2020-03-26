0cf37a4d924188192df06a11005fc2e721b05b53
Tomas Kaplan <tomas.kaplan@gmail.com>
2020-03-26 12:30:11 +0100

    pokus1

--- OK1XOELOG/MainWindow.xaml.cs
+++ OK1XOELOG/MainWindow.xaml.cs
@@ -24,5 +24,26 @@ namespace OK1XOELOG
         {
             InitializeComponent();
         }
+
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            OK1XOELOG.HRD_My_LogbookDataSet hRD_My_LogbookDataSet = ((OK1XOELOG.HRD_My_LogbookDataSet)(this.FindResource("hRD_My_LogbookDataSet")));
+            // Načte data do tabulky TABLE_HRD_CONTACTS_V01. Tento kód lze upravovat podle potřeby.
+            OK1XOELOG.HRD_My_LogbookDataSetTableAdapters.TABLE_HRD_CONTACTS_V01TableAdapter hRD_My_LogbookDataSetTABLE_HRD_CONTACTS_V01TableAdapter = new OK1XOELOG.HRD_My_LogbookDataSetTableAdapters.TABLE_HRD_CONTACTS_V01TableAdapter();
+            hRD_My_LogbookDataSetTABLE_HRD_CONTACTS_V01TableAdapter.Fill(hRD_My_LogbookDataSet.TABLE_HRD_CONTACTS_V01);
+            System.Windows.Data.CollectionViewSource tABLE_HRD_CONTACTS_V01ViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("tABLE_HRD_CONTACTS_V01ViewSource")));
+            tABLE_HRD_CONTACTS_V01ViewSource.View.MoveCurrentToFirst();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.textBlock = MainWindow.textBox + MainWindow.textBox_Copy;
+        }
     }
 }
